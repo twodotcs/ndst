e5fd3fc41cd289ee6b29e65003d4ecf67e37c15d
twodot <ndt@twodotco.uk>
2019-03-27 22:20:25 +0000

    v0.04 - added usb devices (lsusb)

--- ndst.py
+++ ndst.py
@@ -12,8 +12,20 @@ disk = run('df -hl', shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
 user = run("whoami", shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
 uptime = run("uptime -p", shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
 date = run('date', shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
-x = (cpu[2] + '\n' + cpu[15] + '\n' + cpu[16] + '\n' + cpu[17] + '\n' + cpu[19] + '\n' + cpu[20] + '\n' + cpu[21] + '\n' + cpu[22] + '\n' + cpu[23] + '\n')[:-1]
+#
+usb = run("lsusb", shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')[:-1].split('\n')
+x = 0
+no_id = ''
+while(x<int(len(usb))-1):
+      line = usb[x]
+      sep_id = line.split('ID')
+      no_id  = sep_id[0] + sep_id[1][11:] + '\n' + no_id
+      x = x + 1
+usb = no_id[:-1]
+#
 print(neo)
+x = (cpu[2] + '\n' + cpu[15] + '\n' + cpu[16] + '\n' + cpu[17] + '\n' + cpu[19] + '\n' + cpu[20] + '\n' + cpu[21] + '\n' + cpu[22] + '\n' + cpu[23] + '\n')[:-1]
 print(x)
 print('Date: ' + date[:-1])
-print('Disk Usage: \n' + disk)
+print('Disk Usage: \n' + disk[:-1])
+print('USB Devices: \n' + usb)
