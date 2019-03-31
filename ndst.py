40d9df8636130bc1ed91489629c4f6191161b388
twodot <ndt@twodotco.uk>
2019-03-31 00:04:05 +0000

    v0.1(0.05) - just made it look nicer, removed uneeded stuff

--- ndst.py
+++ ndst.py
@@ -1,18 +1,43 @@
 import sys
 from subprocess import run
 import subprocess
-distro = run('cat /etc/*release', shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8').split('\n')[0].split('"')[1]
-ram = run('vmstat -s -S M', shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
-totram = int(ram.split('\n')[0].split('M')[0])
-freeram = int(ram.split('\n')[1].split('M')[0])
-ram_used = int(totram - freeram)
+def whitegen(n):
+      x = ''
+      while(len(x)<n):
+            x = x + ' '
+      return x
+
 cpu  = run("lscpu", shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8').split('\n')
-neo = run("neofetch --gtk2 off --gtk3 off --refresh_rate on --disable wm de --cpu_cores physical --stdout --shell_path on --memory_percent on", shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')[:-2]
+cpu = (cpu[2] + '\n' + cpu[15] + '\n' + cpu[16] + '\n' + cpu[17] + '\n' + cpu[19] + '\n' + cpu[20] + '\n' + cpu[21] + '\n' + cpu[22] + '\n' + cpu[23] + '\n')[:-1]
+#NEOBEGIN
+neo = run("neofetch --gtk2 off --gtk3 off --refresh_rate on --disable wm de --cpu_cores physical --stdout --memory_percent on", shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')[:-2].split('\n')
+print(neo[0]  + '\n' +  neo[1])
+itr = 2
+lst = []
+while(itr<len(neo)):
+      line = neo[itr]
+      linelist = line.split(':')
+      itt = 0
+      while(itt<len(linelist)):
+            lst.append(linelist[itt])
+            itt = itt + 1
+      itr = itr + 1
+itr = 0
+while(itr<len(lst)):  
+      n = 21
+      line = lst[itr] + ':' + lst[itr+1][1]
+      x = 0 
+      if len(line) != n:
+            x = n - len(line)
+            col = ':' + whitegen(x)
+            line = lst[itr] + col + lst[itr+1]
+            if(itr % 2 == 0):
+                  print(line)
+      itr = itr + 2
+#NEOEND          
 disk = run('df -hl', shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
-user = run("whoami", shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
-uptime = run("uptime -p", shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
 date = run('date', shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
-#
+#USBBEGIN
 usb = run("lsusb", shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')[:-1].split('\n')
 x = 0
 no_id = ''
@@ -22,10 +47,8 @@ while(x<int(len(usb))-1):
       no_id  = sep_id[0] + sep_id[1][11:] + '\n' + no_id
       x = x + 1
 usb = no_id[:-1]
-#
-print(neo)
-x = (cpu[2] + '\n' + cpu[15] + '\n' + cpu[16] + '\n' + cpu[17] + '\n' + cpu[19] + '\n' + cpu[20] + '\n' + cpu[21] + '\n' + cpu[22] + '\n' + cpu[23] + '\n')[:-1]
-print(x)
+####USBEND
+print(cpu)
 print('Date: ' + date[:-1])
 print('Disk Usage: \n' + disk[:-1])
 print('USB Devices: \n' + usb)
