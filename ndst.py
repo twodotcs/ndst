bdb015d60044ad807dd1ac6abd84b429a781021f
twodot <ndt@twodotco.uk>
2019-04-02 21:42:12 +0100

    v0.11(0.6) - tidied it up a bit, removed overengineered parts

--- ndst.py
+++ ndst.py
@@ -1,40 +1,28 @@
 import sys
 from subprocess import run
 import subprocess
-def whitegen(n):
-      x = ''
-      while(len(x)<n):
-            x = x + ' '
-      return x
-
 cpu  = run("lscpu", shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8').split('\n')
 cpu = (cpu[2] + '\n' + cpu[15] + '\n' + cpu[16] + '\n' + cpu[17] + '\n' + cpu[19] + '\n' + cpu[20] + '\n' + cpu[21] + '\n' + cpu[22] + '\n' + cpu[23] + '\n')[:-1]
+
 #NEOBEGIN
 neo = run("neofetch --gtk2 off --gtk3 off --refresh_rate on --disable wm de --cpu_cores physical --stdout --memory_percent on", shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')[:-2].split('\n')
 print(neo[0]  + '\n' +  neo[1])
-itr = 2
-lst = []
-while(itr<len(neo)):
-      line = neo[itr]
+neolist = []
+for iterator in range(0,len(neo)):
+      line = neo[iterator]
       linelist = line.split(':')
-      itt = 0
-      while(itt<len(linelist)):
-            lst.append(linelist[itt])
-            itt = itt + 1
-      itr = itr + 1
-itr = 0
-while(itr<len(lst)):  
-      n = 21
-      line = lst[itr] + ':' + lst[itr+1][1]
-      x = 0 
-      if len(line) != n:
-            x = n - len(line)
-            col = ':' + whitegen(x)
-            line = lst[itr] + col + lst[itr+1]
-            if(itr % 2 == 0):
-                  print(line)
-      itr = itr + 2
-#NEOEND          
+      list_iter = 0
+      if(iterator >= 2):
+            while(list_iter<len(linelist)):
+                  neolist.append(linelist[list_iter])
+                  list_iter+=1      
+iterator = 0
+for iterator in range(0,len(neolist),2):
+      width = 22
+      line = neolist[iterator] + ':' + neolist[iterator+1][1]
+      div = ':'.ljust(width - len(line))
+      print(neolist[iterator] + div + neolist[iterator+1])
+#NEOEND
 disk = run('df -hl', shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
 date = run('date', shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
 #USBBEGIN
