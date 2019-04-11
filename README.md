d80e920b37b4bab62041801f6e082f9868f67691
twodot <ndt@twodotco.uk>
2019-04-11 23:33:28 +0100

    v0.5 - Uploading to dpaste is now a feature, added --no-deps and --help

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # NDST (ndt-stat)  
 <img src="https://github.com/twodotcs/ndst/raw/master/ndst.png"></br>  
-A script to get detailed system info.  
+A metascript to get system info.  
 all you need is:  
 - python (>=3.5.2, lower might work)
 - neofetch (whatever version stdout works in, test being implemented soon)  
-- cut, can be run with --no-cut to avoid this. You probably have it.
+- cut, can be run with --no-deps to avoid this. You probably have it.
+- curl, for the uploading to dpaste. (optional)    
 ## Features
 - lsusb (usb)
 - lscpu (cpu)
 - df (all disk usage)  
+- uploads to dpaste  
 - Sensors and stuff coming soon, when I can figure out how to parse them...  
-## why python?
+## Why Python?
 I don't know bash. Rewrite it yourself if you want to.
--- ndst.py
+++ ndst.py
@@ -1,8 +1,9 @@
 import sys
-from subprocess import run
+import io
+from contextlib import redirect_stdout
 import subprocess
 def shexec(x):
-      return run(x, shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
+      return subprocess.check_output(x, shell=True, stderr=True).decode('utf-8')
 def usb_nocut():
       usb = shexec("lsusb")[:-1].split('\n')
       x = 0
@@ -14,9 +15,11 @@ def usb_nocut():
             x = x + 1
       usb = no_id[:-1]
       return usb
+
 def usb_cut():
        usb = shexec("lsusb | cut -d ' '  -f 1-4,7-")
        return usb
+ 
 def neo_nocut():
       neo = shexec("neofetch --gtk2 off --gtk3 off --refresh_rate on --disable wm de --cpu_cores physical --stdout --memory_percent on")[:-2].split('\n')
       print(neo[0]  + '\n' +  neo[1])
@@ -35,38 +38,45 @@ def neo_nocut():
             line = neolist[iterator] + ':' + neolist[iterator+1][1]
             div = ':'.ljust(width - len(line))
             print(neolist[iterator] + div + neolist[iterator+1])
+            
 def neo_cut():
       neolist1 = shexec("neofetch --gtk2 off --gtk3 off --refresh_rate on --disable wm de --cpu_cores physical --stdout --memory_percent on | cut -d ':' -f 1").split('\n')
-      print(neolist1[0]  + '\n' +  neolist1[1])
       width = 20
       iterator = 2
       neolist2 = shexec("neofetch --gtk2 off --gtk3 off --refresh_rate on --disable wm de --cpu_cores physical --stdout --memory_percent on | cut -d ':' -f 2").split('\n')
+      print(neolist1[0]  + '\n' +  neolist1[1]) ##first 2 lines
       while(iterator<len(neolist2)-2):
             line = neolist1[iterator] + ':'.ljust(width - len(neolist1[iterator])) + neolist2[iterator]
             print(line)
             iterator+=1
             
 cpu  = shexec("lscpu").split('\n')
-cpu = (cpu[2] + '\n' + cpu[15] + '\n' + cpu[16] + '\n' + cpu[17] + '\n' + cpu[19] + '\n' + cpu[20] + '\n' + cpu[21] + '\n' + cpu[22] + '\n' + cpu[23] + '\n')[:-1]
+cpu = (cpu[2] + '\n' + cpu[15] + '\n' + cpu[16] + '\n' + cpu[17] + '\n' + cpu[19] + '\n')
 disk = shexec('df -hl')
 date = shexec('date')
+payload_nocut = cpu + 'Date: ' + date + 'Disk Usage: \n' + disk + 'USB Devices: \n' + usb_nocut()
 try:
       if sys.argv[1] == '--no-cut':
-            neo_nocut()
-            print(cpu)
-            print('Date: ' + date[:-1])
-            print('Disk Usage: \n' + disk[:-1])
-            print('USB Devices: \n' + usb_nocut())
+            f = io.StringIO()
+            with redirect_stdout(f):
+                  neo_nocut()
+            x = f.getvalue()
+            payload = x +  payload_nocut
+            print(payload)
+            term = input('log to dpaste? (requires cURL)')
+            if term == 'yes':
+                  x = shexec("curl -s -F \"content=" + payload + '\" http://dpaste.com/api/v2/')
+                  print("URL: " + x)
+      if sys.argv[1] == '--no-deps':
+            print(payload_nocut)
+      elif sys.argv[1] == '--help':
+            print('ndst - a tool to get system info. run with --no-deps for usage without neofetch or cut, or --no-cut for if you just dont have cut for some reason')
       else:
-            neo_cut()
-            print(cpu)
-            print('Date: ' + date[:-1])
-            print('Disk Usage: \n' + disk[:-1])
-            print('USB Devices: \n' + usb_cut())
+            print('invalid args')
 except IndexError:
-      neo_cut()
-      print(cpu)
-      print('Date: ' + date[:-1])
-      print('Disk Usage: \n' + disk[:-1])
-      print(usb_cut())
-      
+     payload = str(neo_cut())[:-4] + str(cpu) + '\nDate: ' + date + 'Disk Usage:\n' + disk + str(usb_cut())
+     print(payload)
+     term = input('log to dpaste? (requires cURL)')
+     if term == 'yes':
+            x = shexec("curl -s -F \"content=" + payload + '\" http://dpaste.com/api/v2/')
+            print("URL: " + x)
